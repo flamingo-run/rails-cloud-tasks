52f70fd9a8602bd1258eff73972357fd0023bb8c
Guilherme Araújo <guilhermeama@gmail.com>
2021-01-06 17:42:17 -0300

    Add specs

---
+++ spec/rails_cloud_tasks/job_spec.rb
@@ -0,0 +1,84 @@
+require 'active_support/core_ext'
+require 'google/cloud/tasks/v2'
+
+describe RailsCloudTasks::Job do
+  let(:instance) { instance_spy(described_class) }
+
+  describe 'perform_now' do
+    before do
+      allow(described_class).to receive(:new).and_return(instance)
+
+      described_class.perform_now
+    end
+
+    it 'executes the job immediatelly' do
+      expect(instance).to have_received(:perform)
+    end
+  end
+
+  describe 'perform_later' do
+    subject(:perform_later) { described_class.perform_later }
+
+    before do
+      allow(described_class).to receive(:enqueue_task).with(nil).and_return('task-id')
+    end
+
+    it { is_expected.to eq 'task-id' }
+  end
+
+  describe 'perform_in' do
+    subject(:perform_in) { described_class.perform_in(1.minute) }
+
+    before do
+      allow(described_class).to receive(:enqueue_task)
+        .with(nil, instance_of(Integer))
+        .and_return('task-id')
+    end
+
+    it { is_expected.to eq 'task-id' }
+  end
+
+  describe 'perform_at' do
+    subject(:perform_at) { described_class.perform_at(1.hour.from_now) }
+
+    before do
+      allow(described_class).to receive(:enqueue_task)
+        .with(nil, instance_of(Integer))
+        .and_return('task-id')
+    end
+
+    it { is_expected.to eq 'task-id' }
+  end
+
+  describe 'queue management' do
+    subject(:enqueue_task) { described_class.perform_in(3.minutes) }
+
+    let(:client) { instance_spy(Google::Cloud::Tasks::V2::CloudTasks::Client) }
+    let(:queue_path) { 'projetcs/project_id/location/location_id/queues/queue_id' }
+
+    before do
+      allow(described_class).to receive(:client).and_return(client)
+      allow(client).to receive(:queue_path).and_return(queue_path)
+    end
+
+    context 'when the queue does not exist' do
+      before do
+        first_call = true
+
+        allow(client).to receive(:create_task) do
+          if first_call
+            first_call = false
+            raise Google::Cloud::FailedPreconditionError
+          end
+          OpenStruct.new(name: 'task-id')
+        end
+
+        enqueue_task
+      end
+
+      it 'creates the queue' do
+        expect(client).to have_received(:create_queue)
+      end
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,40 @@
+require 'bundler/setup'
+
+require File.expand_path('test_app/config/environment.rb', __dir__)
+
+require 'pry'
+require 'rspec/its'
+require 'rspec/rails'
+require 'shoulda-matchers'
+require 'simplecov'
+
+require 'rails-cloud-tasks'
+
+formatters = [SimpleCov::Formatter::HTMLFormatter]
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(formatters)
+
+if ENV['COVERAGE']
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+end
+
+RSpec.configure do |config|
+  config.include RailsCloudTasks::Engine.routes.url_helpers
+  config.include Rails.application.routes.url_helpers
+
+  config.infer_spec_type_from_file_location!
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
+RailsCloudTasks.configure do |config|
+  config.project_id = 'test-project'
+  config.location_id = 'us-central1'
+  config.queue_id = 'test-queue'
+  config.base_url = 'https://test.com'
+
+  config.register_jobs([RailsCloudTasks::Job])
+end
